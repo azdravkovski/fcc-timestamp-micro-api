48d447b91aa616e2aa6a76ec43c1381d10f2ac81
Glitch (maze-scooter) <none>
2018-01-18 15:38:37 +0000

    🎢👁 Checkpoint package.json:531052/73 server.js:531052/23

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const http = request('http');
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
